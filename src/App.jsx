a4a3c60efb23142ee6759b8035529ba8ae69dc39
sghufranmehdi <sgmehdi110@gmail.com>
2023-11-17 00:23:12 +0500

    version1.0 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/AppName";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2";
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo />
+      <div className="item-container">
+        <TodoItem1 />
+        <TodoItem2 />
+      </div>
+    </center>
+  );
 }
 
 export default App;
---
+++ src/Components/AppName/index.jsx
@@ -0,0 +1,5 @@
+const AppName = () => {
+  return <h1>Pro Tasker</h1>;
+};
+
+export default AppName;
